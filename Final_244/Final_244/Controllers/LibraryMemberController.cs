c29d4c56f600b6b6c83f95ad4aef0aa36346e6b0
AdelineCIT <114101362+AdelineCIT@users.noreply.github.com>
2023-11-21 13:11:14 -0500

    database
    connected to database

---
+++ Final_244/Final_244/Controllers/LibraryMemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_244.Controllers
+{
+    public class LibraryMemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Final_244/Final_244/Models/CheckOut.cs
+++ Final_244/Final_244/Models/CheckOut.cs
@@ -5,11 +5,11 @@ namespace Final_244.Models
     public class CheckOut
     {
         public int Id { get; set; }
+        public int BookId { get; set; }
+        public int LibraryMemberId { get; set; } 
         public DateTime CheckedOutDate { get; set; }
         public DateTime DueDate { get; set; }
         public DateTime? ReturnedDate { get; set; }
-        public int BookId { get; set; }
-        public int LibraryMemberId { get; set; }
         //associations to one book and one member
         public Book book { get; set; }
         public LibraryMember Member { get; set; }
